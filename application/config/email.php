24b3600c66f2f0ae7133158051244029491eb9f7
Hindrik Deelstra <h.deelstra@oxilion.nl>
2017-01-18 11:37:05 +0100

    Add custom configs for email and database, autoload dbconf

---
+++ application/config/email.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array('protocol'      => "smtp",
+                'smtp_host'     => "localhost",
+                'smtp_user'     => "snackz@snack.oxilion.nl",
+                'smtp_pass'     => "snackz!",
+                'smtp_port'     => "25"
+                );
+
+?>
+
